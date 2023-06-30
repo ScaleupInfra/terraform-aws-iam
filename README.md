9d9b6041ea325553ec6833481f3ac579dc23cc61
tusharkumar <tusharkumar27864@gmail.com>
2023-06-30 12:19:55 +0530

    creating IAM user through terraform

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# terraform-aws-iam-policy
+# Code Explanation: IAM User Setup
+This code is used to set up user accounts and permissions for a system. Let's break it down step by step:
+
+### Step 1: Creating a Group
+
+First, we create a group called "autoscaleupinfra". A group is a collection of users with similar access needs. You all will be part of this group.
+
+### Step 2: Listing Users
+
+Next, we have a list of users who will have access to the system. We have ten users in total, each with their own names or email addresses.
+
+### Step 3: Creating Users
+
+Now, we create user accounts for each of you based on the names or email addresses provided. You will have your own individual accounts to log in.
+
+### Step 4: Setting User Login Profiles
+
+To enable you to log in to your accounts, we set up login profiles and generate passwords for each user. You don't have to worry about changing the passwords for now.
+
+### Step 5: Creating an EC2 Role
+
+We create a special role called "ec2-role". This role defines what you can do within the system and what actions you're allowed to perform.
+
+### Step 6: Defining EC2 Policies
+
+Two policies are defined: one for creating EC2 instances and another for creating ECR repositories. Policies are like rules that determine what you can do.
+
+### Step 7: Attaching Policies to the EC2 Role
+
+We attach the EC2 and ECR policies to the "ec2-role". This ensures that you, as users with this role, can follow the defined rules and perform the necessary actions.
+
+### Step 8: Granting Full Access to Users
+
+Good news! Each one of you is granted full access to EC2. We attach a policy called "AmazonEC2FullAccess" to your user accounts, allowing you to perform all EC2-related actions.
+
+### Step 9: Granting Full Access to the Group
+
+Since you are all part of the "autoscaleupinfra" group, we also grant the entire group full access to EC2. This means any user within the group can perform EC2 actions without restrictions.
+
+### Step 10: Creating a File with User Passwords
+
+For your convenience, we create a file called "iam_user_passwords.txt". It contains your usernames and passwords, serving as a reference for you to remember your login credentials.
+
+### Step 11: Outputting User Passwords
+
+Lastly, we provide an output that displays your usernames and passwords. This output helps you quickly find and access your login credentials whenever needed.
+
+## That's it! This code sets up your user accounts, defines permissions, and ensures you have the necessary access to work within the system.  
--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
+# Create an IAM group named "autoscaleupinfra"
 resource "aws_iam_group" "my_group" {
   name = "autoscaleupinfra"
 }
 
+# Define a list of user names
 locals {
   users = [
     "learning@infrasity.com",
@@ -17,11 +19,13 @@ locals {
   ]
 }
 
+# Create IAM users using the names from the list
 resource "aws_iam_user" "my_users" {
   count = length(local.users)
   name  = local.users[count.index]
 }
 
+# Configure login profiles for IAM users with generated passwords
 resource "aws_iam_user_login_profile" "my_users_login_profile" {
   count                   = length(local.users)
   user                    = aws_iam_user.my_users[count.index].name
@@ -29,6 +33,7 @@ resource "aws_iam_user_login_profile" "my_users_login_profile" {
   password_reset_required = false
 }
 
+# Create an IAM role named "ec2-role" for EC2 instances
 resource "aws_iam_role" "ec2_role" {
   name               = "ec2-role"
   assume_role_policy = <<EOF
@@ -48,6 +53,7 @@ resource "aws_iam_role" "ec2_role" {
 EOF
 }
 
+# Create an IAM policy named "ec2-policy" for EC2 instance management
 resource "aws_iam_policy" "ec2_policy" {
   name        = "ec2-policy"
   description = "Allows creating EC2 instances"
@@ -67,6 +73,7 @@ resource "aws_iam_policy" "ec2_policy" {
 EOF
 }
 
+# Create an IAM policy named "ecr-policy" for ECR repository management
 resource "aws_iam_policy" "ecr_policy" {
   name        = "ecr-policy"
   description = "Allows creating ECR repositories"
@@ -86,33 +93,39 @@ resource "aws_iam_policy" "ecr_policy" {
 EOF
 }
 
+# Attach the EC2 policy to the EC2 role
 resource "aws_iam_role_policy_attachment" "ec2_policy_attachment" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = aws_iam_policy.ec2_policy.arn
 }
 
+# Attach the ECR policy to the EC2 role
 resource "aws_iam_role_policy_attachment" "ecr_policy_attachment" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = aws_iam_policy.ecr_policy.arn
 }
 
+# Attach the AmazonEC2FullAccess policy to the IAM users
 resource "aws_iam_user_policy_attachment" "ec2_full_access_attachment" {
   count      = length(local.users)
   user       = aws_iam_user.my_users[count.index].name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
+# Attach the AmazonEC2FullAccess policy to the IAM group
 resource "aws_iam_group_policy_attachment" "ec2_full_access_attachment" {
   group      = aws_iam_group.my_group.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
+# Create a local file containing the IAM user passwords
 resource "local_file" "iam_user_passwords" {
   count    = length(local.users)
   filename = "iam_user_passwords.txt"
   content  = "Username: ${aws_iam_user.my_users[count.index].name}\nPassword: ${aws_iam_user_login_profile.my_users_login_profile[count.index].password}"
 }
 
+# Output the user passwords as a map
 output "user_passwords" {
   value = {
     for i, user in local.users : user => aws_iam_user_login_profile.my_users_login_profile[i].password
