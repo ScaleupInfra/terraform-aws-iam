1da9183581a8e85c3d69f599a6e3186dc27b6a0b
tusharkumar <tusharkumar27864@gmail.com>
2023-07-01 19:38:05 +0530

    initial code

---
+++ iam-module/main.tf
@@ -0,0 +1,78 @@
+# Create an IAM group named "autoscaleupinfra"
+resource "aws_iam_group" "my_group" {
+  name = var.iam_group_name
+}
+
+# Define a list of user names
+locals {
+  users = var.user_names
+}
+
+# Create IAM users using the names from the list
+resource "aws_iam_user" "my_users" {
+  count = length(local.users)
+  name  = local.users[count.index]
+}
+
+# Configure login profiles for IAM users with generated passwords
+resource "aws_iam_user_login_profile" "my_users_login_profile" {
+  count                   = length(local.users)
+  user                    = aws_iam_user.my_users[count.index].name
+  password_length         = var.password_length
+  password_reset_required = var.password_reset_required
+}
+
+# Create an IAM role named "ec2-role" for EC2 instances
+resource "aws_iam_role" "ec2_role" {
+  name               = var.ec2_role_name
+  assume_role_policy = var.ec2_role_assume_role_policy
+}
+
+# Create an IAM policy named "ec2-policy" for EC2 instance management
+resource "aws_iam_policy" "ec2_policy" {
+  name        = var.ec2_policy_name
+  description = var.ec2_policy_description
+
+  policy = var.ec2_policy
+}
+
+# Create an IAM policy named "admin-policy" for administrative privileges
+resource "aws_iam_policy" "admin_policy" {
+  name        = var.admin_policy_name
+  description = var.admin_policy_description
+
+  policy = var.admin_policy
+}
+
+# Attach the EC2 policy to the IAM group
+resource "aws_iam_group_policy_attachment" "ec2_policy_attachment" {
+  group      = aws_iam_group.my_group.name
+  policy_arn = aws_iam_policy.ec2_policy.arn
+}
+
+# Attach the admin policy to the IAM group
+resource "aws_iam_group_policy_attachment" "admin_policy_attachment" {
+  group      = aws_iam_group.my_group.name
+  policy_arn = aws_iam_policy.admin_policy.arn
+}
+
+# Attach the EC2 policy to the IAM users
+resource "aws_iam_user_group_membership" "ec2_policy_group_membership" {
+  count  = length(local.users)
+  user   = aws_iam_user.my_users[count.index].name
+  groups = [aws_iam_group.my_group.name]
+}
+
+# Create an IAM policy named "ecr-policy" for ECR repository management
+resource "aws_iam_policy" "ecr_policy" {
+  name        = var.ecr_policy_name
+  description = var.ecr_policy_description
+
+  policy = var.ecr_policy
+}
+
+# Attach the ECR policy to the EC2 role
+resource "aws_iam_role_policy_attachment" "ecr_policy_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ecr_policy.arn
+}
--- main.tf
+++ main.tf
@@ -1,78 +1,100 @@
-# Create an IAM group named "autoscaleupinfra"
-resource "aws_iam_group" "my_group" {
-  name = var.iam_group_name
+module "iam" {
+  source = "./iam-module"
+
+  # the default values for the variables
+  iam_group_name  = "autoscaleupinfra"
+  user_names =  [
+    "learning@infrasity.com",
+    "user2",
+    "user3",
+    "user4",
+    "user5",
+    "user6",
+    "user7",
+    "user8",
+    "user9",
+    "user10",
+    "admin_user1",
+    "admin_user2"
+  ]
+  password_length         = 16
+  password_reset_required = false
+  ec2_role_name  = "ec2-role"
+  ec2_role_assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+         "AWS": "*"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
 }
-
-# Define a list of user names
-locals {
-  users = var.user_names
+EOF
+  ec2_policy_name = "ec2-policy"
+  ec2_policy_description = "Allows specific EC2 actions in ap-southeast-1 region and denies other instance types"
+  ec2_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:*"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "StringEquals": {
+          "aws:RequestedRegion": "ap-southeast-1"
+        }
+      }
+    },
+    {
+      "Effect": "Deny",
+      "Action": "ec2:*",
+      "Resource": "*",
+      "Condition": {
+        "ForAnyValue:StringNotLike": {
+          "ec2:InstanceType": [
+            "t2.micro"
+          ]
+        }
+      }
+    }
+  ]
 }
-
-# Create IAM users using the names from the list
-resource "aws_iam_user" "my_users" {
-  count = length(local.users)
-  name  = local.users[count.index]
+EOF
+  admin_policy_name ="admin-policy"
+  admin_policy_description = "Administrative policy"
+  admin_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "*",
+      "Resource": "*"
+    }
+  ]
 }
-
-# Configure login profiles for IAM users with generated passwords
-resource "aws_iam_user_login_profile" "my_users_login_profile" {
-  count                   = length(local.users)
-  user                    = aws_iam_user.my_users[count.index].name
-  password_length         = var.password_length
-  password_reset_required = var.password_reset_required
+EOF
+  ecr_policy_name = "ecr-policy"
+  ecr_policy_description = "Allows creating ECR repositories"
+  ecr_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowECR",
+      "Effect": "Allow",
+      "Action": "ecr:*",
+      "Resource": "*"
+    }
+  ]
 }
-
-# Create an IAM role named "ec2-role" for EC2 instances
-resource "aws_iam_role" "ec2_role" {
-  name               = var.ec2_role_name
-  assume_role_policy = var.ec2_role_assume_role_policy
-}
-
-# Create an IAM policy named "ec2-policy" for EC2 instance management
-resource "aws_iam_policy" "ec2_policy" {
-  name        = var.ec2_policy_name
-  description = var.ec2_policy_description
-
-  policy = var.ec2_policy
-}
-
-# Create an IAM policy named "admin-policy" for administrative privileges
-resource "aws_iam_policy" "admin_policy" {
-  name        = var.admin_policy_name
-  description = var.admin_policy_description
-
-  policy = var.admin_policy
-}
-
-# Attach the EC2 policy to the IAM group
-resource "aws_iam_group_policy_attachment" "ec2_policy_attachment" {
-  group      = aws_iam_group.my_group.name
-  policy_arn = aws_iam_policy.ec2_policy.arn
-}
-
-# Attach the admin policy to the IAM group
-resource "aws_iam_group_policy_attachment" "admin_policy_attachment" {
-  group      = aws_iam_group.my_group.name
-  policy_arn = aws_iam_policy.admin_policy.arn
-}
-
-# Attach the EC2 policy to the IAM users
-resource "aws_iam_user_group_membership" "ec2_policy_group_membership" {
-  count  = length(local.users)
-  user   = aws_iam_user.my_users[count.index].name
-  groups = [aws_iam_group.my_group.name]
-}
-
-# Create an IAM policy named "ecr-policy" for ECR repository management
-resource "aws_iam_policy" "ecr_policy" {
-  name        = var.ecr_policy_name
-  description = var.ecr_policy_description
-
-  policy = var.ecr_policy
-}
-
-# Attach the ECR policy to the EC2 role
-resource "aws_iam_role_policy_attachment" "ecr_policy_attachment" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = aws_iam_policy.ecr_policy.arn
+EOF
 }
