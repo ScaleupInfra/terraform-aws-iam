4bb13536c0c0bf2311dd78b741420e99e75def32
tusharkumar <tusharkumar27864@gmail.com>
2023-07-03 16:22:33 +0530

    policy.json added

--- main.tf
+++ main.tf
@@ -26,4 +26,8 @@ module "iam" {
   admin_policy_description = "Administrative policy"
   ecr_policy_name = "ecr-policy"
   ecr_policy_description = "Allows creating ECR repositories"
+  admin_policy                     = "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Effect\": \"Allow\",\n      \"Action\": \"*\",\n      \"Resource\": \"*\"\n    }\n  ]\n}"
+  ecr_policy                       = "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Sid\": \"AllowECR\",\n      \"Effect\": \"Allow\",\n      \"Action\": \"ecr:*\",\n      \"Resource\": \"*\"\n    }\n  ]\n}"
+  ec2_role_assume_role_policy      = "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Sid\": \"\",\n      \"Effect\": \"Allow\",\n      \"Principal\": {\n         \"AWS\": \"*\"\n      },\n      \"Action\": \"sts:AssumeRole\"\n    }\n  ]\n}"
+  ec2_policy                       = "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Effect\": \"Allow\",\n      \"Action\": [\n        \"ec2:*\"\n      ],\n      \"Resource\": \"*\",\n      \"Condition\": {\n        \"StringEquals\": {\n          \"aws:RequestedRegion\": \"ap-southeast-1\"\n        }\n      }\n    },\n    {\n      \"Effect\": \"Deny\",\n      \"Action\": \"ec2:*\",\n      \"Resource\": \"*\",\n      \"Condition\": {\n        \"ForAnyValue:StringNotLike\": {\n          \"ec2:InstanceType\": [\n            \"t2.micro\"\n          ]\n        }\n      }\n    }\n  ]\n}"
 }
